15ddc3b44d7846087f9119ea5ba8d50f7be7cc20
Oriane <oriane.daley@gmail.com>
2018-10-16 22:56:24 -0400

    Move coffee place data to its own file + do not store in App state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import mapboxgl from 'mapbox-gl';
 import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+import * as CoffeePlaces from './CoffeePlaces';
 //var mbxGeocoding = require('@mapbox/mapbox-gl-geocoder');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
@@ -13,8 +14,7 @@ class App extends Component {
     this.state = {
       lng: -74.0226071,
       lat: 40.7786204,
-      zoom: 9.5,
-      locations_geocoded: []
+      zoom: 9.5
     };
   }
 
@@ -31,20 +31,6 @@ class App extends Component {
       zoom
     });
 
-    // coffee places
-    const coffeePlaces = [
-      {
-        name: 'Madman Espresso',
-        type: 'cafe',
-        address: '54 University Pl, New York, NY 10003'
-      },
-      {
-        name: 'Allendale Eats',
-        type: 'diner',
-        address: '101 w allendale ave, allendale, new jersey 07401'
-      }
-    ]
-
     // create empty feature collection
     const geojson = {
       type: 'FeatureCollection',
@@ -55,36 +41,34 @@ class App extends Component {
     const geocodingClient = mbxGeocoding({ accessToken: mapboxgl.accessToken });
 
     // forward geocode each coffee place address
-    coffeePlaces.forEach((place) => {
+    CoffeePlaces.coffeePlaces.forEach((place) => {
       geocodingClient.forwardGeocode({
         query: place.address,
         limit: 1
       })
         .send()
         .then(response => {
-          // store feature object
-          const feature = response.body.features[0];
-          // add title and description properties
-          feature.properties = {
-            title: place.name,
-                  description: place.type
+          if (response != null && response.error == null) {
+            // store feature object
+            const feature = response.body.features[0];
+            console.log(feature);
+            // add title and description properties
+            feature.properties = {
+              title: place.name,
+              description: place.type
+            }
+            // push feature object to feature collection
+            geojson.features.push(feature);
+          } else {
+            return ;
           }
-          // push feature object to feature collection
-          geojson.features.push(feature);
 
-          // TODO: handle more than one promise before setting state?
-          // TODO: move to componentDidUpdate?
-
-          // update state with geocoded locations
-          this.setState({
-            locations_geocoded: geojson.features
-          })
           // create and add a marker for each location to the map
-          this.state.locations_geocoded.forEach((location) => {
+          geojson.features.forEach((location) => {
             new mapboxgl.Marker()
               .setLngLat(location.geometry.coordinates)
               .setPopup(new mapboxgl.Popup({ offset: 25 })
-              .setHTML(`<h3>${location.properties.title}</h3><p>${location.properties.description}</p>`))
+                .setHTML(`<h3>${location.properties.title}</h3><p>${location.properties.description}</p>`))
               .addTo(map);
           });
 
---
+++ src/CoffeePlaces.js
@@ -0,0 +1,13 @@
+// coffee places
+export const coffeePlaces = [
+    {
+      name: 'Madman Espresso',
+      type: 'cafe',
+      address: '54 University Pl, New York, NY 10003'
+    },
+    {
+      name: 'Allendale Eats',
+      type: 'diner',
+      address: '101 w allendale ave, allendale, new jersey 07401'
+    }
+  ]
