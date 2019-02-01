78005aca3dcd35a14dae888d58fda79dc0a4d8aa
Oriane <oriane.daley@gmail.com>
2019-01-31 21:23:02 -0500

    Updates

---
+++ precache-manifest.ff913fdc785d6ac572bbbcdb416c4a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26456dfea7fb67273d71661544cf135a",
+    "url": "/coffee-map/static/media/coffee-solid.26456dfe.svg"
+  },
+  {
+    "revision": "5b2241d8a4bf5ff710c4",
+    "url": "/coffee-map/static/js/runtime~main.5b2241d8.js"
+  },
+  {
+    "revision": "9c4bdce877b29bf91d19",
+    "url": "/coffee-map/static/js/main.9c4bdce8.chunk.js"
+  },
+  {
+    "revision": "a3678a809b6567f9e47d",
+    "url": "/coffee-map/static/js/1.a3678a80.chunk.js"
+  },
+  {
+    "revision": "9c4bdce877b29bf91d19",
+    "url": "/coffee-map/static/css/main.a81292f6.chunk.css"
+  },
+  {
+    "revision": "4ab2622b9d3ecc175d86c9d003e6a624",
+    "url": "/coffee-map/index.html"
+  }
+];
